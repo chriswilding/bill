148c2cfc9cb846f9c36f0700d26f27597a7fbcb5
Chris Wilding <hello@chriswilding.co.uk>
2016-03-13 17:56:44 +0000

    Add spec for Api::BillsController show action

--- spec/controllers/api/bills_controller_spec.rb
+++ spec/controllers/api/bills_controller_spec.rb
@@ -1,4 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Api::BillsController, type: :controller do
+  describe 'GET #show' do
+    it 'responds with a HTTP 200 status code' do
+      get :show, id: 1
+      expect(response).to be_success
+      expect(response).to have_http_status(200)
+    end
+  end
 end
