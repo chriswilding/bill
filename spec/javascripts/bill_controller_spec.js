04f1f144e288de5c1c5adc9b5d212c96f0746966
Chris Wilding <hello@chriswilding.co.uk>
2016-03-13 21:26:27 +0000

    Add spec to check JSON API reponse is assigned to the scope
    Also, remove old controller initialization test

--- spec/javascripts/bill_controller_spec.js
+++ spec/javascripts/bill_controller_spec.js
@@ -1,14 +1,30 @@
 describe('BillCtrl', function() {
   beforeEach(module('bill'));
 
-  var $controller;
+  var scope;
+  var response = {
+    statement: {
+      generated: '2015-01-11',
+      due: '2015-01-25',
+      period: {
+        from: '2015-01-26',
+        to: '2015-02-25'
+      }
+    }
+  }
 
-  beforeEach(inject(function(_$controller_) {
-    $controller = _$controller_;
+  beforeEach(inject(function(_$httpBackend_, $rootScope, $controller) {
+    $httpBackend = _$httpBackend_;
+    $httpBackend.expectGET('api/bills/1').respond(response);
+    scope = $rootScope.$new();
+    controller = $controller('BillCtrl', { $scope: scope });
   }));
 
-  it('can initialise the BillCtrl', function() {
-    var $scope = {};
-    var controller = $controller('BillCtrl', { $scope: $scope });
+  it('assigns the JSON API response to the scope', function() {
+    expect(scope.bill).toBeUndefined();
+
+    $httpBackend.flush();
+
+    expect(scope.bill).toEqual(response);
   });
 });
