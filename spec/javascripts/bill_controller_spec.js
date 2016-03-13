ae327999203a50c81825867c5c8c02e295f9e843
Chris Wilding <hello@chriswilding.co.uk>
2016-03-13 19:40:53 +0000

    Add spec to check BillCtrl can be initialised

--- spec/javascripts/bill_controller_spec.js
+++ spec/javascripts/bill_controller_spec.js
@@ -1,3 +1,14 @@
 describe('BillCtrl', function() {
   beforeEach(module('bill'));
+
+  var $controller;
+
+  beforeEach(inject(function(_$controller_) {
+    $controller = _$controller_;
+  }));
+
+  it('can initialise the BillCtrl', function() {
+    var $scope = {};
+    var controller = $controller('BillCtrl', { $scope: $scope });
+  });
 });
