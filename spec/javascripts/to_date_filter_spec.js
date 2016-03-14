754e6783ca1f3e810943ec885e03c66ded9cd8fa
Chris Wilding <hello@chriswilding.co.uk>
2016-03-14 17:50:45 +0000

    Complete to_date_filter_spec

--- spec/javascripts/to_date_filter_spec.js
+++ spec/javascripts/to_date_filter_spec.js
@@ -3,6 +3,9 @@ describe('toDate filter', function() {
 
   it('should convert an ISO 8601 formatted string to a date', function() {
     inject(function(toDateFilter) {
+      // Month param to Date constructor is zero indexed
+      var expected = new Date(2016, 2, 14).getTime();
+      expect(toDateFilter('2016-03-14')).toEqual(expected);
     });
   });
 });
