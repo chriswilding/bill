a1e2b7570a0d52e6fc742d008d24b743d5bf8f4a
Chris Wilding <hello@chriswilding.co.uk>
2016-03-14 20:18:38 +0000

    Add some explanatory comments

--- app/controllers/api/bills_controller.rb
+++ app/controllers/api/bills_controller.rb
@@ -3,6 +3,10 @@ require 'net/http'
 module Api
   class BillsController < ApplicationController
     def show
+      # In a real application I would expect to create/use a http client class
+      # to adapt the API and help handle expections. The below assumes that the
+      # JSON response is always valid and does not attempt to handle an HTTP
+      # errors as you would expect in a real application.
       uri = URI('http://safe-plains-5453.herokuapp.com/bill.json')
       render json: Net::HTTP.get(uri)
     end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
   root 'home#index'
 
   namespace :api do
+    # As there is only a single bill a single `resource` would also work.
+    # However, I've used resources as you would normally expected to have
+    # muliple bills. The controller ignore the id and always returns the same
+    # JSON
     resources :bills, only: [:show]
   end
 end
