b793192e782a4a4cc5f7fb0432dc52e2e8bf5568
Chris Wilding <hello@chriswilding.co.uk>
2016-03-14 17:42:57 +0000

    Add empty toDate filter test

---
+++ spec/javascripts/to_date_filter_spec.js
@@ -0,0 +1,8 @@
+describe('toDate filter', function() {
+  beforeEach(module('bill'));
+
+  it('should convert an ISO 8601 formatted string to a date', function() {
+    inject(function(toDateFilter) {
+    });
+  });
+});
